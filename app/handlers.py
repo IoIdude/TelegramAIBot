47ea4acb4437e2f84734408bc434ae9c672899a4
IoIdude <gleb22002e@mail.ru>
2024-05-26 02:06:32 +0300

    Add chat action

--- app/handlers.py
+++ app/handlers.py
@@ -1,6 +1,7 @@
-from main import dp
+from main import dp, client
 from aiogram.fsm.context import FSMContext
 from aiogram import Router, F
+from aiogram.enums.chat_action import ChatAction
 from aiogram.types import Message
 from app.opengpt import answer
 from fsm.states import WaitState
@@ -16,6 +17,7 @@ async def wait_print_answer(message: Message):
 @router.message(F.text)
 async def print_answer(message: Message, state: FSMContext):
     await state.set_state(WaitState.msgPrinted)
+    await client.send_chat_action(message.chat.id, ChatAction.TYPING)
     resp = await answer(message.text)
     await message.answer(resp)
     await state.clear()
